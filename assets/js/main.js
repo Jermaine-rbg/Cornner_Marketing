f102b6dea18582cad988e7d1e8d3175da146d41c
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-03-08 20:33:26 -0500

    updated main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Resi - v4.6.0
-* Template URL: https://bootstrapmade.com/resi-free-bootstrap-html-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
